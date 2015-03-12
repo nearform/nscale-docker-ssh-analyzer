7a85aa09d04778de2114be401b3ec599a4567334
Matteo Collina <matteo.collina@gmail.com>
2015-03-12 15:32:11 +0000

    Use batch mode and no host checks for sh scripts.

--- scripts/getContainers.sh
+++ scripts/getContainers.sh
@@ -1,4 +1,4 @@
-ssh -t -i "$2" "$1@$3" <<'ENDSSH'
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -o ConnectTimeout=10 -t -i "$2" "$1@$3" <<'ENDSSH'
 echo -e "GET /containers/json HTTP/1.0\r\n" | nc -U /var/run/docker.sock
 
 ENDSSH
--- scripts/pre.sh
+++ scripts/pre.sh
@@ -1,4 +1,4 @@
-ssh -t -i "$2" "$1@$3" <<'ENDSSH'
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -o ConnectTimeout=10 -t -i "$2" "$1@$3" <<'ENDSSH'
 echo --ready--
 
 ENDSSH
